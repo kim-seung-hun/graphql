d0213626c812b97ca8e9f8b0ec172d052e3aed09
kim-seung-hun <rlatmdgns16@naver.com>
2023-02-01 19:06:42 +0900

    study

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,28 +1,98 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+  {
+    id: "1",
+    text: "first one!",
+    userId: "2",
+  },
+  {
+    id: "2",
+    text: "second one!",
+    userId: "1",
+  },
+];
+
+let user = [
+  {
+    id: "1",
+    firstName: "김",
+    lastName: "승훈",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
+  },
+];
+
 // GET /text
 // GET /hello
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    firstName: String!
+    lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
-    psotTweet(text: String!, userId: ID!): Tweet!
+    postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allUsers() {
+      return user;
+    },
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(root, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(root, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return user.find((user) => user.id === userId);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 // Apollo Server requires either an existing schema, modules or typeDefs
 // >> error 발생
